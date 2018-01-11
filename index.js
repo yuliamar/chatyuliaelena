b314d382f21f74213c1898319b7729102d8d4cdf
yuliamar <vasya2605@yandex.ru>
2018-01-11 22:43:14 +0100

    visual recognision

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var redis = require('socket.io-redis');
-io.adapter(redis({ host: 'https://chataplication.eu-de.mybluemix.net/', port: 6379 }));
+io.adapter(redis({ host: 'https://chataplication.eu-de.mybluemix.net', port: 6379 }));
 
 //var redis = require('socket.io-redis');
 //io.adapter(redis({ host: host, port: port }));
